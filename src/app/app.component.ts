0077f85be07d4d181d68c03c136d42b079e93292
Mohammad Dilshad <mohdilsh@publicisgroupe.net>
2024-10-25 00:32:15 +0530

    initial design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MeetingComponent } from "./components/meeting/meeting.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MeetingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
